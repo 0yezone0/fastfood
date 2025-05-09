59f0c6b67b1a801a4ebcd6ce58a976d74680ee4a
Vo Dinh Kiet <vokiet2534@gmail.com>
2025-05-09 21:18:20 +0700

    abc

--- fastfood/src/App.js
+++ fastfood/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import FastFoodMenu from './pages/FastFoodMenu';
 import OrderPage from './getTableID.js'
 
-const tableID = OrderPage()
+// const tableID = OrderPage()
 
 const App = () => {
   return <FastFoodMenu />;
